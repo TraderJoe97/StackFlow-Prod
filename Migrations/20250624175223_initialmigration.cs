b1732e4e3b5b5c8be480bbc178f355c46f1c1b2c
Joseph Monakedi <jimonakedi@gmail.com>
2025-06-24 19:53:42 +0200

    fixed migration errors

---
+++ Migrations/20250624175223_initialmigration.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StackFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    role_name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    role_description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    password = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    role_id = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "date", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_users_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "projects",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    project_name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    project_description = table.Column<string>(type: "text", nullable: false),
+                    project_status = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    project_start_date = table.Column<DateTime>(type: "date", nullable: true),
+                    project_due_date = table.Column<DateTime>(type: "date", nullable: true),
+                    created_by = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_projects", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_projects_users_created_by",
+                        column: x => x.created_by,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tickets",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ticket_title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ticket_description = table.Column<string>(type: "text", nullable: false),
+                    assigned_to = table.Column<int>(type: "int", nullable: true),
+                    ticket_status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ticket_priority = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ticket_created_at = table.Column<DateTime>(type: "date", nullable: false),
+                    ticket_due_date = table.Column<DateTime>(type: "date", nullable: false),
+                    ticket_completed_at = table.Column<DateTime>(type: "date", nullable: false),
+                    project_id = table.Column<int>(type: "int", nullable: false),
+                    ticket_created_by = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tickets", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_tickets_projects_project_id",
+                        column: x => x.project_id,
+                        principalTable: "projects",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tickets_users_assigned_to",
+                        column: x => x.assigned_to,
+                        principalTable: "users",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_tickets_users_ticket_created_by",
+                        column: x => x.ticket_created_by,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "comments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ticket_id = table.Column<int>(type: "int", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    created_by = table.Column<int>(type: "int", nullable: false),
+                    comment_created_at = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_comments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_comments_tickets_ticket_id",
+                        column: x => x.ticket_id,
+                        principalTable: "tickets",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_comments_users_created_by",
+                        column: x => x.created_by,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_created_by",
+                table: "comments",
+                column: "created_by");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_ticket_id",
+                table: "comments",
+                column: "ticket_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_projects_created_by",
+                table: "projects",
+                column: "created_by");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tickets_assigned_to",
+                table: "tickets",
+                column: "assigned_to");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tickets_project_id",
+                table: "tickets",
+                column: "project_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tickets_ticket_created_by",
+                table: "tickets",
+                column: "ticket_created_by");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_role_id",
+                table: "users",
+                column: "role_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "comments");
+
+            migrationBuilder.DropTable(
+                name: "tickets");
+
+            migrationBuilder.DropTable(
+                name: "projects");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+        }
+    }
+}
