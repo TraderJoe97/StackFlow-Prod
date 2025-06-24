7f13f156fb79e90e8ea249ec34537ee1e27d1037
KaraboRamotalaDev <KaraboRamotalaDev@gmail.com>
2025-06-24 11:52:30 +0200

    Project initial structure and files

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StackFlow.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
