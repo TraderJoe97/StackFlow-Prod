bf104c7a62156c8134d9b9c5993ce1c52b763e7d
Joseph Monakedi <jimonakedi@gmail.com>
2025-06-30 18:37:11 +0200

    create one dashboard that renders based on role

--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace StackFlow.ViewModels
+﻿using StackFlow.Models;
+using System.Collections.Generic;
+
+namespace StackFlow.ViewModels
 {
     public class DashboardViewModel
     {
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public int CurrentUserId { get; set; }
+
+        public List<Ticket> AssignedToMeTickets { get; set; }
+        public List<Ticket> ToDoTickets { get; set; }
+        public List<Ticket> InProgressTickets { get; set; }
+        public List<Ticket> InReviewTickets { get; set; }
+        public List<Ticket> CompletedTickets { get; set; }
+
+        public List<Project> Projects { get; set; }
     }
 }
