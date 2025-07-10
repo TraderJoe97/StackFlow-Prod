e1fe30c881c4d693bc7b29d8b652b09aa9766bbd
siphokazi <naledimuhauli@gmail.com>
2025-07-10 16:36:20 +0200

    Add TicketComments API controller with JWT auth

--- ApiControllers/TicketsAPIController.cs
+++ ApiControllers/TicketsAPIController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StackFlow.Data;
+using StackFlow.Models;
+using StackFlow.ApiControllers.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace StackFlow.ApiControllers
 {
-    public class TicketsAPIController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
+    [ApiController]
+    [Route("api/ticketcomments")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class TicketCommentsApiController : ControllerBase
